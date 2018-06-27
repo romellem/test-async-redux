b3499f4a0c03530575b53a47fb07228b846f0c12
romellem <romellem.git@gmail.com>
2018-06-26 20:05:40 -0500

    Init's initial files
    My two presentational components are going to be an ArticleList, and an
    Article.
    
    The container component will be generated by Redux, and will be
    implemented a bit later on.

---
+++ src/Article.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class ArticleList extends Component {
+    render() {
+        return <div>Article List</div>;
+    }
+}
+
+export default ArticleList;
