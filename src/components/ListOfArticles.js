d4758d048cc7fe88942aa0f5eab1d0073fabc7d2
romellem <romellem.git@gmail.com>
2018-06-26 22:18:29 -0500

    Adjust header

--- src/components/ListOfArticles.js
+++ src/components/ListOfArticles.js
@@ -3,7 +3,7 @@ import { connect } from 'react-redux';
 
 class Article extends Component {
     render() {
-        return <div>Article List</div>;
+        return <h1>List of Articles:</h1>;
     }
 }
 
