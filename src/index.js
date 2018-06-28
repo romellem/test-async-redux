81e60d807ab274f41fd8455e1225d8bbe57a2781
romellem <romellem.git@gmail.com>
2018-06-28 11:00:56 -0500

    Exposes store to connected components via a Provider

--- src/index.js
+++ src/index.js
@@ -2,11 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 
+import { store } from './redux/store';
+import { Provider } from 'react-redux';
 import { HashRouter } from 'react-router-dom';
 
 ReactDOM.render(
-    <HashRouter>
-        <App />
-    </HashRouter>,
+    <Provider store={store}>
+        <HashRouter>
+            <App />
+        </HashRouter>
+    </Provider>,
     document.getElementById('root')
 );
