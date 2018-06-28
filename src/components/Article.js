f38d193cabc04630b8b0f3b3c0b8633a40538f73
romellem <romellem.git@gmail.com>
2018-06-28 10:57:14 -0500

    Creates "dummy" container component for Articles
    I'm not really displaying much information here, but
    the main logic is present.
    
    One key thing that I didn't have before is my `componentDidUpdate`
    lifecycle. This is what allows me to change the URL and have the
    component update.

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,14 +1,50 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { fetchArticleById } from '../redux/actions';
 
-class ArticleList extends Component {
+const mapStateToProps = state => ({
+    articlesById: state.articlesById,
+});
+const mapDispatchToProps = dispatch => ({
+    onFetchArticleById: id => dispatch(fetchArticleById(id)),
+});
+
+class Article extends Component {
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        let { id } = this.props.match.params;
+        if (prevProps.match.params.id !== id) {
+            this.props.onFetchArticleById(id);
+        }
+    }
+    componentDidMount() {
+        let { id } = this.props.match.params;
+        this.props.onFetchArticleById(id);
+    }
     render() {
         // Get ID from Route
         let { id } = this.props.match.params;
+        let article = this.props.articlesById[id];
+
+        if (!article) {
+            return (
+                <React.Fragment>
+                    {/* <h4>Loading...</h4> */}
+                    <pre>{JSON.stringify(this.props, null, '  ')}</pre>
+                </React.Fragment>
+            );
+        }
+
         return (
-            <h1>Article {id}</h1>
+            <React.Fragment>
+                <h1>{article.title}</h1>
+                <h5>Article {id}</h5>
+                <div dangerouslySetInnerHTML={{ __html: article.body }} />
+            </React.Fragment>
         );
     }
 }
 
-export default ArticleList;
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(Article);
