bf130919cc0705d3eca05842052be427489b4b48
romellem <romellem.git@gmail.com>
2018-06-28 09:15:49 -0500

    Creates reducer for LIST ARTICLES action
    Here, I am going with Option 2 from http://nikolay.rocks/2017-06-18-better-redux-loading,
    which isn't 100% recommended but I'll go with it for now.
    
    I do think the issue of "what if `loading: true` and `data: [ ...items]`?
    How do you handle that "split" state?
    
    I may look to create a new file in my redux directory: initialStates.js
    and export all of those to be used in my reducers as well as my components.
    
    It would be nice to not have to re-instantiate a new object every time
    I want to reduce my action to a new state...
    
    I'll look to implementing that at a later date.

---
+++ src/redux/reducers.js
@@ -0,0 +1,39 @@
+import { combineReducers } from 'redux';
+import {
+    LIST_ARTICLES_REQUEST,
+    LIST_ARTICLES_FAILURE,
+    LIST_ARTICLES_SUCCESS,
+} from './actionTypes';
+
+const listArticles = (state = { loading: false, data: [], error: null }, action) => {
+    switch (action.type) {
+        case LIST_ARTICLES_REQUEST:
+            return {
+                ...state,
+                loading: true,
+            };
+        case LIST_ARTICLES_SUCCESS:
+            return {
+                ...state,
+                data: {
+                    ...state.data,
+                    ...action.payload,
+                },
+                loading: false,
+                error: null,
+            };
+        case LIST_ARTICLES_FAILURE:
+            return {
+                ...state,
+                loading: false,
+                error: action.payload,
+            };
+        default:
+            return state;
+    }
+};
+
+
+export default combineReducers({
+    listArticles,
+});
