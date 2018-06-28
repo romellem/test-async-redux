c92c6932d732265c4c9bd07044dfeba9fc7fd69c
romellem <romellem.git@gmail.com>
2018-06-28 11:00:35 -0500

    Similar dummy container component for List of Articles

--- src/components/ListOfArticles.js
+++ src/components/ListOfArticles.js
@@ -1,10 +1,42 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { fetchArticlesList } from '../redux/actions';
 
-class Article extends Component {
+const mapStateToProps = state => ({
+    listArticles: {
+        ...state.listArticles
+    },
+});
+const mapDispatchToProps = dispatch => ({
+    onFetchArticles: () => dispatch(fetchArticlesList()),
+});
+
+class ListOfArticles extends Component {
+    componentDidMount() {
+        this.props.onFetchArticles();
+    }
     render() {
-        return <h1>List of Articles:</h1>;
+        let data;
+        try {
+            data = JSON.stringify(this.props.listArticles.data);
+        } catch (e) {
+            data = this.props.listArticles.data.toString();
+        }
+        return (
+            <React.Fragment>
+                <h1>List of Articles:</h1>
+                <pre>{JSON.stringify(this.props, null, '  ')}</pre>
+                {/*<ul>
+                    <li>Loading: {this.props.loading.toString()}</li>
+                    <li>Error: {this.props.error.toString()}</li>
+                    <li>Data: {data}</li>
+                </ul>*/}
+            </React.Fragment>
+        );
     }
 }
 
-export default Article;
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(ListOfArticles);
