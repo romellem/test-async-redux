e3f29807bee79744f9c1e17485417aaa1520e1e4
romellem <romellem.git@gmail.com>
2018-06-28 10:06:40 -0500

    Creates reducer for SPECIFIC ARTICLE action
    This reducer will request an article and cache it in our redux state,
    so the next time we request that article, it won't have to perform
    another API request.

--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -3,6 +3,10 @@ import {
     LIST_ARTICLES_REQUEST,
     LIST_ARTICLES_FAILURE,
     LIST_ARTICLES_SUCCESS,
+    
+    FETCH_ARTICLE_REQUEST,
+    FETCH_ARTICLE_FAILURE,
+    FETCH_ARTICLE_SUCCESS,
 } from './actionTypes';
 
 const listArticles = (state = { loading: false, data: [], error: null }, action) => {
@@ -33,7 +37,37 @@ const listArticles = (state = { loading: false, data: [], error: null }, action)
     }
 };
 
+const cacheArticles = (state = { loading: false, error: null, data: {} }, action) => {
+    switch (action.type) {
+        case FETCH_ARTICLE_REQUEST:
+            return {
+                ...state,
+                loading: true,
+            };
+        case FETCH_ARTICLE_SUCCESS:
+            return {
+                ...state,
+                data: {
+                    ...state.data,
+                    [action.payload.id]: {
+                        ...action.payload
+                    },
+                },
+                loading: false,
+                error: null,
+            };
+        case FETCH_ARTICLE_FAILURE:
+            return {
+                ...state,
+                loading: false,
+                error: action.payload,
+            };
+        default:
+            return state;
+    }
+};
 
 export default combineReducers({
     listArticles,
+    articlesById: cacheArticles,
 });
