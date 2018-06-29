057a25fcf16add4aeba38917aeceefab57487c87
romellem <romellem.git@gmail.com>
2018-06-29 14:47:57 -0500

    Enable redux to use Redux DevTools

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,13 @@
-import { applyMiddleware, createStore } from 'redux';
+import { applyMiddleware, createStore, compose } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
+
+// Allow Chrome extension "Redux DevTools" to function properly
+// @link https://github.com/zalmoxisus/redux-devtools-extension
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 // prettier-ignore
 export const store = createStore(
     rootReducer,
-    applyMiddleware(thunk)
+    composeWithDevTools(applyMiddleware(thunk))
 );
